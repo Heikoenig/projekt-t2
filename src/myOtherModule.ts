d1fced20b945091e64b151316955e55cf50a1495
Heikoenig <hei.koenig.21@lehre.mosbach.dhbw.de>
2022-05-11 17:55:17 +0200

    Seite länger + Design
    Button war vorher direkt am Footer

--- src/myOtherModule.ts
+++ src/myOtherModule.ts
@@ -1,5 +1,3 @@
-// THIS IS A MODULE
-
 export function alertMe(): void {
   alert("SPIELREGELN - Ordne die Länder nach ihrer Größe - Clicke auf den Button, um die Reihenfolge zu kontrollieren - Grün= Richtig - Rot=Falsch");
 }
